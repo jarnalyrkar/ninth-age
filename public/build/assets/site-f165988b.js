af683aab0677e0784badd925ab740b77add7050e
jarnalyrkar <joachimsolstrom@gmail.com>
2024-08-26 21:06:51 +0200

    Update to 5.23 of statamic. Add container-mixin

---
+++ public/build/assets/site-f165988b.js
@@ -0,0 +1,2 @@
+class e{constructor(){this.button=document.querySelector("#menu_button"),this.button&&this.button.addEventListener("click",this.openMenu)}openMenu(){console.log("Consider me open")}}document.addEventListener("DOMContentLoaded",()=>{new e});
+//# sourceMappingURL=site-f165988b.js.map
